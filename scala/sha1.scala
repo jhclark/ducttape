7eaa99e014126a49ee3152e13a3e2e4a74edf566
Jonathan Clark <jon.h.clark@gmail.com>
2011-05-26 18:05:25 -0400

    Make code more readable

--- scala/sha1.scala
+++ scala/sha1.scala
@@ -1,5 +1,6 @@
 import java.security.MessageDigest
 
+// problem: this currently only works for files < 2gb due to Java's mmap deficiencies 
 object FileUtils {
   import java.io.File
   import java.io.FileInputStream
@@ -12,11 +13,11 @@ object FileUtils {
     val fileSize = file.length
     val stream = new FileInputStream(file)
     val buf = stream.getChannel.map(READ_ONLY, 0, fileSize)
-    var arr = new Array[Byte](SIZE);
+    var arr = new Array[Byte](SIZE)
     while(buf.hasRemaining) {
       if(buf.remaining < arr.length)
         arr = new Array[Byte](buf.remaining)
-      buf.get(arr, 0, arr.length);
+      buf.get(arr, 0, arr.length)
       f(arr)
     }
     stream.close
@@ -29,7 +30,7 @@ class Digest(alg: String) {
   def update(bytes: Array[Byte]) = dig.update(bytes)
   def digest() = dig.digest
   override def equals(that: Any) = that match { case other: Digest => (this.digest == other.digest) }
-  override def toString = digest.map(0xFF & _).map { "%02x".format(_) }.foldLeft(""){_ + _}
+  override def toString = digest.map(a => "%02x".format(a & 0xFF)).mkString("")
 }
 
 val dig = new Digest("sha1")
