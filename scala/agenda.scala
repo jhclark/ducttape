006932362e0ce2861f68b990c37c60f1fb904aec
Jonathan Clark <jon.h.clark@gmail.com>
2011-05-30 19:12:05 -0400

    Broken unpacked DAG walking code. Split out data structures from walkters

--- scala/agenda.scala
+++ scala/agenda.scala
@@ -1,92 +1,10 @@
 package ducttape.hyperdag
 
-import collection._
+
 import scala.collection.JavaConversions._
 import java.util.concurrent._
 
 import ducttape.Types._
-import ducttape.viz._
-
-class HyperEdge[H,E](private val id: Int, val h: H, val e: List[E]) {
-  override def hashCode = id
-  override def equals(that: Any) = that match { case other: HyperEdge[_,_] => (other.id == this.id) }
-  override def toString = h.toString + " " + e.toString
-}
-
-class PackedVertex[V](private val id: Int, val value: V) {
-  override def hashCode = id
-  override def equals(that: Any) = that match { case other: PackedVertex[_] => (other.id == this.id) }
-  override def toString = value.toString
-}
-
-// immutable
-class PackedDag[V,H,E](val roots: List[PackedVertex[V]],
-                       val vertices: List[PackedVertex[V]],
-                       private val inEdgesMap: Map[PackedVertex[V], Seq[HyperEdge[H,E]]],
-                       private val outEdgesMap: Map[PackedVertex[V], Seq[HyperEdge[H,E]]],
-                       private val edges: Map[HyperEdge[H,E], (List[PackedVertex[V]],PackedVertex[V])]) {
-                       
-  val size: Int = vertices.size
-
-  def walker(): PackedDagWalker[V]
-    = new PackedDagWalker[V](this)
-  def inEdges(v: PackedVertex[V]): Seq[HyperEdge[H,E]]
-    = inEdgesMap.getOrElse(v, Seq.empty)
-  def outEdges(v: PackedVertex[V]): Seq[HyperEdge[H,E]]
-    = outEdgesMap.getOrElse(v, Seq.empty)
-  def parents(v: PackedVertex[V]): Seq[PackedVertex[V]] = for(e <- inEdges(v); src <- sources(e)) yield src
-  def children(v: PackedVertex[V]): Seq[PackedVertex[V]]
-    = for(e <- outEdges(v)) yield sink(e)
-  def sources(e: HyperEdge[H,E]): Seq[PackedVertex[V]] = edges(e)._1
-  def sink(e: HyperEdge[H,E]): PackedVertex[V] = edges(e)._2
-
-  def toGraphViz(): String = {
-    val str = new StringBuilder(1000)
-    str ++= "digraph G {\n"
-    for(v <- vertices) {
-      for(ant <- parents(v)) {
-        str ++= GraphViz.escape(ant.toString) + " -> " + GraphViz.escape(v.toString) + "\n"
-      }
-    }
-    str ++= "}\n"
-    str.toString
-  }
-}
-
-class PackedDagBuilder[V,H,E] {
-
-  private val vertices = new mutable.HashSet[PackedVertex[V]]
-  private val inEdges = new mutable.HashMap[PackedVertex[V],mutable.ListBuffer[HyperEdge[H,E]]]
-  private val outEdges = new mutable.HashMap[PackedVertex[V],mutable.ListBuffer[HyperEdge[H,E]]]
-  private val edges = new mutable.HashMap[HyperEdge[H,E], (List[PackedVertex[V]],PackedVertex[V])]
-  private var vertexId = 0
-  private var edgeId = 0
-
-  // before adding hyperparents we must already know the realizations?
-  // this seems to defeat the purpose of the builder...
-  def add(v: V): PackedVertex[V] = {
-    val pv = new PackedVertex[V](vertexId, v)
-    vertices += pv
-    vertexId += 1
-    pv
-  }
-
-  def add(h: H, e: List[E], sources: List[PackedVertex[V]], sink: PackedVertex[V]): HyperEdge[H,E] = {
-    require(sources.forall(v => vertices(v)), "Add sources first")
-    require(vertices(sink), "Add sink first")
-    val he = new HyperEdge[H,E](edgeId, h, e)
-    edgeId += 1
-    for(src <- sources) outEdges.getOrElseUpdate(src, new mutable.ListBuffer) += he
-    inEdges.getOrElseUpdate(sink, new mutable.ListBuffer) += he
-    edges += he -> (sources, sink)
-    he
-  }
-
-  def build() = {
-    val roots = for(v <- vertices if !inEdges.contains(v)) yield v
-    new PackedDag[V,H,E](roots.toList, vertices.toList, inEdges.toMap, outEdges.toMap, edges.toMap)
-  }
-}
 
 // agenda-based DAG iterator that allows for parallelization
 class PackedDagWalker[V](dag: PackedDag[V,_,_]) extends Walker[PackedVertex[V]] {
@@ -141,8 +59,8 @@ class PackedDagWalker[V](dag: PackedDag[V,_,_]) extends Walker[PackedVertex[V]] 
     for(consequent <- dag.children(key.v)) {
       val activeCon = active.getOrElseUpdate(consequent, new ActiveVertex(consequent))
       var allFilled = true
+      val antecedents = dag.parents(consequent)
       for(i <- 0 until activeCon.filled.size) {
-        val antecedents = dag.parents(consequent)
         if(key.v == antecedents(i)) {
           activeCon.filled(i) = key
           } else if(activeCon.filled(i) == null) {
@@ -163,25 +81,73 @@ class PackedDagWalker[V](dag: PackedDag[V,_,_]) extends Walker[PackedVertex[V]] 
   }
 }
 
-// this interface explicitly avoids giving unpacked vertices as
-// parents so that we can eventually discard more of the explored space
-class UnpackedVertex[V,H,E](val packed: PackedVertex[V],
-                            val edge: HyperEdge[H,E],
-                            val realization: List[H],
-                            val parentRealizations: List[List[H]]);
+// hedge filter allows us to exclude certain hyperedges from
+// the edge derivation (e.g. edges without a realization name)
+// the "edge derivation" == the realization
+// TODO: SPECIFY GOAL VERTICES
+class UnpackedDagWalker[V,H,E](val dag: PackedDag[V,H,E],
+                               val hedgeFilter: H => Boolean,
+                               val selectionFilter: MultiSet[H] => Boolean)
+  extends Walker[UnpackedVertex[V,H,E]] {
 
-class UnpackedDagWalker[V,H,E](val dag: PackedDag[V,H,E])
-  extends Walker[UnpackedVertex[V,H,E]]{
+    class ActiveVertex(val v: PackedVertex[V],
+                       val e: Option[HyperEdge[H,E]]) {
 
-  class ActiveVertex(val v: PackedVertex[V],
-                     val realization: List[H]) {
+    // accumulate parent realizations
+    // indices: sourceEdge, whichUnpacking, whichRealization
+    val filled = new Array[Set[Set[H]]](if(e.isEmpty) 0 else e.get.e.size)
 
-//    val filled = new Array[(PackedVertex[V],E,List[RInst])]()
-    def toUnpacked() = new UnpackedVertex[V,H,E](v, realization, filled.toList)
+    private def unpack(i: Int,
+                       iFixed: Int,
+                       combo: MultiSet[H],
+                       parentReals: Array[Set[H]],
+                       callback: Seq[H] => Unit) = {
+
+      // hedgeFilter has already been applied
+      if(i == filled.size) {
+        if(selectionFilter(combo)) {
+          callback(new UnpackedVertex[V,H,E](v, e,
+                         combo.toList, parentReals.toList))
+        }
+      } else if(i == iFixed) {
+          unpack(i+1, iFixed, combo, parentReals, callback)
+      } else {
+        // for each backpointer to a realization...
+        for(parentRealization: Set[H] <- filled(i)) {
+          combo ++= parentRealization
+          parentReals(i) = parentRealization
+          unpack(i+1, iFixed, combo, parentReals, callback)
+          combo --= parentRealization
+        }     
+      }
+    }
+
+    def unpack(iFixed: Int,
+               fixedRealization: Set[H],
+               callback: Seq[H] => Unit): Seq[Set[H]] = {
+
+      val combo = new MultiSet[H]
+      val parentReals = new Array[Set[H]](filled.size)
+      parentReals(iFixed) = fixedRealization
+      combo += fixedRealization
+      unpack(0, iFixed, fixedRealization, combo, parentReals, callback)
+    }
+    
+    // get the *new* unpackings possible based on all
+    // backpointers we've saved so far, but fixing
+    // one of the hyperedge sources at a particular
+    // realization
+/*
+    def unpack(): Seq[UnpackedVertex[V,H,E]] = {
+      realization = new mutable.Set[H]
+      for(parentSet <- filled)
+        realization ++= parentSet
+      if(!e.isEmpty && hedgeFilter(e.get))
+        realization += e.get
+      val v = dag.sink(e)
+      new UnpackedVertex[V,H,E](v, e, realization, filled.toList)
   }
-
-  // TODO: PackingVertex spawns a ??? for each hyperedge completed
-  // TODO: VanillaVertex spawns a ??? for each full incoming combo
+*/
 
   private val active = new mutable.HashMap[UnpackedVertex[V,H,E],ActiveVertex]
                          with mutable.SynchronizedMap[UnpackedVertex[V,H,E],ActiveVertex]
@@ -192,7 +158,7 @@ class UnpackedDagWalker[V,H,E](val dag: PackedDag[V,H,E])
 
   // first, visit the roots
   for(root <- dag.roots.iterator) {
-    val actRoot = new ActiveVertex(root)
+    val actRoot = new ActiveVertex(root, None, List.empty)
     agenda.offer(actRoot)
     active += root -> actRoot
   }
@@ -213,19 +179,42 @@ class UnpackedDagWalker[V,H,E](val dag: PackedDag[V,H,E])
     require(active.contains(item), "Cannot find active vertex for %s in %s".format(item, active))
     val key: ActiveVertex = active(item)
 
+    // TODO: Are elements of taken properly hashable?
+
     // we always lock agenda & completed jointly
     agenda.synchronized {
       taken -= key
     }
 
-    // 1) see if this completes any hyperedges
+    // first, match fronteir vertices
+    // note: consequent is an edge unlike the packed walker
+    for(consequent <- dag.outEdges(key.v)) {
+      val activeCon = active.getOrElseUpdate(consequent,
+                        new ActiveVertex(consequent, Some(outEdge)))
 
-    // 2) see if this completed hyperedge completes any packing (meta?) edges
+      val antecedents = dag.sources(consequent)
+      for(iEdge <- 0 until activeCon.filled.size) {
+        // save a backpointer to the realizations (hyperedge derivation)
+        // as of this parent antecedent vertex
+        if(item.v == antecedents(iEdge)) {
+          // before adding backpointer, take cross-product
+          // of unpackings possible when holding this realization
+          // fixed
+          activeCon.unpack(iEdge, item.realization,
+            (realization: Seq[H]) => {
+              agenda.synchronized {
+                agenda.offer(activeCon)
+                // TODO: We could sort the agenda
+                // here to impose different objectives...
+              }
+            })
+          activeCon.filled(iEdge) += item.realization
+        }
+      }
+    }
 
-    // 3) for the newly completed packing edge, complete any new UnpackedDagVertices
-    //    that come from the cross product of this new packing edge
-    //    and all other orthogonal unpacked edges; these must be checked
-    //    against the selection to make sure they're valid
-    // Orthogonal: coming from other incoming *packed* edges
+    // finally visit this vertex
+    completed += key
   }
 }
+
