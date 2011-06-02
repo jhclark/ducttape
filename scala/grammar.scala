460623f678cf373e3cea9ad9f82c5c0033cd5644
Jonathan Clark <jon.h.clark@gmail.com>
2011-06-02 13:29:57 -0400

    Add dependencies and targets to grammar

--- scala/grammar.scala
+++ scala/grammar.scala
@@ -3,8 +3,10 @@ import scala.util.parsing.input._
 
 class Task(val name: String,
            val comments: Seq[String],
+           val inputs: Seq[String],
+           val outputs: Seq[String],
            val commands: Seq[String]) {
-  override def toString = name + " " + comments.toString + " " + commands
+  override def toString = name + " " + comments.toString + " " + inputs + " " + outputs + " " + commands
 }
 
 // see http://stackoverflow.com/questions/5063022/use-scala-parser-combinator-to-parse-csv-files
@@ -13,18 +15,32 @@ object MakelikeDSL extends RegexParsers {
 
   var lineNum = 0
 
-  //def eol = "\r\n" | "\n"
-  def eol: Parser[Any] = """(\r?\n)+""".r
-  //val EofCh = CharArrayReader.EofCh
+  class TaskDef(val name: String,
+                val targets: Seq[String],
+                val deps: Seq[String]) {}
+
+  def eol = "\r\n" | "\n" | CharArrayReader.EofCh
+  def space = """[ \t]+""".r
   def taskBlock: Parser[Task] = comments ~ taskDef ~ commands ^^ {
-    case com ~ name ~ cmds => new Task(name, com, cmds)
+    case com ~ td ~ cmds => new Task(td.name, com, td.deps, td.targets, cmds)
   }
 
   def comments: Parser[Seq[String]] = repsep(comment, eol) <~ (eol?)
   def comment: Parser[String] = """#\s*""".r ~> commentContent
   def commentContent: Parser[String] = """[^\r\n]+""".r
 
-  def taskDef: Parser[String] = "[" ~> """[a-zA-Z0-9.-]+""".r <~ "]" ~ eol
+  // deps CANNOT be separated by a newline or else we don't know where commands begin
+  def taskDef: Parser[TaskDef] = taskName ~ taskTargets ~ taskDeps <~ eol ^^ {
+    case name ~ targets ~ deps => new TaskDef(name, targets, deps)
+  }
+  def taskName: Parser[String] = "[" ~> """[a-zA-Z0-9.-]+""".r <~ "]"
+  def taskTargets: Parser[Seq[String]] = repsep(taskTarget, space) 
+  def taskTarget: Parser[String] = """[^\r\n:]+""".r
+  def taskDeps: Parser[Seq[String]] = opt("""\s*:\s*""".r ~> repsep(taskDep, space)) ^^ {
+    case Some(deps) => deps
+    case None => List.empty
+  }
+  def taskDep: Parser[String] = """[^\r\n:]+""".r
 
   def commands: Parser[Seq[String]] = repsep(command, eol) <~ (eol?)
   def command: Parser[String] = """[^\r\n]+""".r
@@ -34,7 +50,8 @@ object MakelikeDSL extends RegexParsers {
     val pos: Position = result.next.pos
     result match {
       case Success(res, _) => println(res)
-      case Failure(msg, _) => println("FAIL on line %d column %d: %s".format(pos.line, pos.column, msg))
+      case Failure(msg, _)
+        => println("FAIL on line %d column %d: %s".format(pos.line, pos.column, msg))
       case Error(msg, _) => println("ERROR: "+msg)
     }
   }
@@ -42,6 +59,6 @@ object MakelikeDSL extends RegexParsers {
 
 MakelikeDSL.doMatch("""# hi
 # how are you ?
-[head-5]
+[head-5] a b c : x y z
 cat""")
 
