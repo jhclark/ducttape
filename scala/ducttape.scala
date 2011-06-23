6061045de25beb6ffeb222b90cc028faf673271b
Jonathan Clark <jon.h.clark@gmail.com>
2011-06-23 10:53:15 -0400

    Usage

--- scala/ducttape.scala
+++ scala/ducttape.scala
@@ -1,11 +1,17 @@
 import ducttape.io._
 import ducttape.util._
+import System._
 
 object Ducttape {
   def main(args: Array[String]) {
     println("%sDuctTape v0.1".format(Console.YELLOW))
     println("%sBy Jonathan Clark".format(Console.BLUE))
     println(Console.RESET)
+
+    if(args.length != 1) {
+      err.println("Usage: ducctape workflow.tape")
+      exit(1)
+    }
 
     var file = args(0)
     println("Reading workflow from %s".format(file))
