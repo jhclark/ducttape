0f45bc975ccacb7827392517a9096a394340dfaa
Jonathan Clark <jon.h.clark@gmail.com>
2011-06-23 10:53:47 -0400

    Execute external process and get output

--- scala/util.scala
+++ scala/util.scala
@@ -29,4 +29,19 @@ object Shell {
     var code = "bash".run(new ProcessIO(provideIn, handleOut, handleErr)).exitValue()
     println("Returned %s".format(code))
   }
+
+  def runGetOutput(cmd: String): String = {
+    def provideIn(x: OutputStream) = {
+      val bash = new PrintStream(x)
+      bash.println("set -eo pipefail")
+      bash.println(cmd)
+      bash.close()
+    }
+    val output = new StringBuilder
+    def handleOut(x: InputStream) = { Source.fromInputStream(x).getLines().foreach( output.append(_) ) }
+    def handleErr(x: InputStream) = { Source.fromInputStream(x).getLines().foreach( println(_) ) }
+    var code = "bash".run(new ProcessIO(provideIn, handleOut, handleErr)).exitValue()
+    println("Returned %s".format(code))
+    output.toString
+  }
 }
