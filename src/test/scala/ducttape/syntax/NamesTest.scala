a05fb82e48e98eedcb23dcfe6565e5009af0a9a8
Lane Schwartz <dowobeha@gmail.com>
2011-08-05 10:17:46 -0400

    Added test cases

---
+++ src/test/scala/ducttape/syntax/NamesTest.scala
@@ -0,0 +1,20 @@
+package ducttape.syntax
+
+import org.scalatest.FlatSpec
+import scala.util.parsing.combinator.Parsers
+
+import ducttape.syntax.AbstractSyntaxTree._
+import ducttape.syntax.Grammar._
+import ducttape.syntax.GrammarParser._
+
+import ducttape.util.Tests._
+
+
+class NamesTest extends FlatSpec {
+
+	"Valid variable names" should "parse successfully" in {
+	
+	}
+  
+  
+}
