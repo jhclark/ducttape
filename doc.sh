c3310d7e3457be5aa7171d78e06dceb7288842fc
Jonathan Clark <jon.h.clark@gmail.com>
2011-08-10 10:10:48 -0400

    Documentation generator updates

--- doc.sh
+++ doc.sh
@@ -3,10 +3,11 @@ set -euo pipefail
 
 docdir=doc
 mkdir -p $docdir
-# TODO: Remove hardcoded version
-scala -cp target/scala-2.9.0.final/classes/ DuctTapeDoc ./syntax/tutorial > $docdir/doc.tex
+# TODO: JAR path?
+scala -cp ducttape.jar DuctTapeDoc ./syntax/tutorial --latex > $docdir/doc.tex
+scala -cp ducttape.jar DuctTapeDoc ./syntax/tutorial --markdown > $docdir/doc.md
 cd $docdir
 pdflatex doc.tex
 pdflatex doc.tex # Get the table of contents right
 
-echo >&2 "Tutorial documentation written to: $docdir/doc.pdf"
+echo >&2 "Tutorial documentation written to: $docdir/doc.pdf and $docdir/doc.md"
