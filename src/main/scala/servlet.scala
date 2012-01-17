ba3cab2f43a959778bf94478cb124ed1f834c468
Jonathan Clark <jon.h.clark@gmail.com>
2012-01-17 13:21:44 -0500

    Fixes for web UI

--- src/main/scala/servlet.scala
+++ src/main/scala/servlet.scala
@@ -1,13 +1,18 @@
 import javax.servlet.http._
+import java.io.File
 
-class WorkflowServlet extends HttpServlet {
+class WorkflowServlet(workflowDirs: Seq[File]) extends HttpServlet {
+  import ducttape.viz._
+
   val db = new WorkflowDatabase("workflow.db")
+  val dotFile = new File(workflowDirs.head, ".xdot") // TODO: This should actually just be .dot
+
   override def init() {}
   override def doGet(req: HttpServletRequest, resp: HttpServletResponse) = service(req, resp)
   override def service(req: HttpServletRequest, resp: HttpServletResponse) { 
     val pw = resp.getWriter
-    // TODO: Compile XDot
-    val xdot = io.Source.fromFile("webui2/xdot").getLines.mkString("\n")
+    val dot = io.Source.fromFile(dotFile.getAbsolutePath).getLines.mkString("\n")
+    val xdot = GraphViz.compileXDot(dot)
     pw.println(xdot)
     pw.flush
   }
@@ -20,16 +25,20 @@ import org.eclipse.jetty.servlet._
 import javax.servlet._
 
 object WebServer extends App {
-  val port = 8080
+  val port = args(0).toInt
+  val workflowsFile = args(1)
   val server = new Server(port)
 
+  val workflows = io.Source.fromFile(workflowsFile).getLines.toList.map(str => new File(str))
+
   val sc = new ServletContextHandler(hl, "/json", false, false)
-  sc.addServlet(new ServletHolder(classOf[WorkflowServlet]), "/")
+  val servlet: Servlet = new WorkflowServlet(workflows)
+  sc.addServlet(new ServletHolder(servlet), "/")
 
   val res = new ResourceHandler
   res.setDirectoriesListed(true)
   res.setWelcomeFiles(Array("index.htm"))
-  res.setResourceBase("webui2/")
+  res.setResourceBase("webui/")
 
   val hl = new HandlerList
   hl.setHandlers(Array(sc, res, new DefaultHandler))
--- webui.sh
+++ webui.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 scala -cp lib/servlet-api-3.0.jar:lib/sqlitejdbc-v056.jar:lib/jetty-all-8.0.4.v20111024.jar:ducttape.jar \
-    WebServer
+    WebServer $@
