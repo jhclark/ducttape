5c60d3dd4582cddf90dea39ddfa04cfbf84aa7b4
Jonathan Clark <jon.h.clark@gmail.com>
2012-01-21 14:55:19 -0500

    Fixup the web UI. Route around silly io.Source issue.

--- webui.sh
+++ webui.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
+scriptDir=$(dirname $0)
 
-scala -cp lib/servlet-api-3.0.jar:lib/sqlitejdbc-v056.jar:lib/jetty-all-8.0.4.v20111024.jar:ducttape.jar \
+JAVA_OPTS='-Xmx128m' scala -cp $scriptDir/lib/servlet-api-3.0.jar:$scriptDir/lib/sqlitejdbc-v056.jar:$scriptDir/lib/jetty-all-8.0.4.v20111024.jar:$scriptDir/ducttape.jar \
     WebServer $@
