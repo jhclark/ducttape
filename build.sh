76dee70ea900a6afa1ad50e65facf14b37550cfb
Jonathan Clark <jon.h.clark@gmail.com>
2012-03-22 12:29:29 -0400

    Update lib paths in build

--- build.sh
+++ build.sh
@@ -5,11 +5,18 @@ libDir=$scriptDir/lib
 
 $scriptDir/get_deps.sh
 
+libs=""
+libs="$libs:$libDir/sqlitejdbc-v056.jar"
+libs="$libs:$libDir/scala-optparse-1.1.jar"
+libs="$libs:$libDir/test/scalatest-1.7.1.jar"
+libs="$libs:$libDir/webui/servlet-api-3.0.jar"
+libs="$libs:$libDir/webui/jetty-all-8.0.4.v20111024.jar"
+
 echo >&2 "Building source..."
 mkdir -p $scriptDir/bin
 find $scriptDir/src/main/scala \
   | egrep '\.scala$' \
-  | xargs fsc -cp $libDir/scalatest-1.6.1.jar:$libDir/sqlitejdbc-v056.jar:$libDir/servlet-api-3.0.jar:$libDir/jetty-all-8.0.4.v20111024.jar  \
+  | xargs fsc -cp $libs  \
     -d $scriptDir/bin/ \
   | $scriptDir/color_scalac.awk
 
