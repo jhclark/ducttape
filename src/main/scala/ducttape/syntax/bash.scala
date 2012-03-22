9b3db6079bb771898fccca5b0c01543121e448a2
Jonathan Clark <jon.h.clark@gmail.com>
2012-03-22 09:33:20 -0400

    Crazy hacky bash parser that does simple matching of paired elements such as {} and also recognizes variables in a variety of contexts.

--- src/main/scala/ducttape/syntax/bash.scala
+++ src/main/scala/ducttape/syntax/bash.scala
@@ -10,33 +10,63 @@ import java.io.File
 
 object BashParser extends App with RegexParsers {
   	
-  println("hi")
 
   override val skipWhitespace = false
+
+  println("heredoc:")
+  val heredocResult: ParseResult[BashCode] = parseAll(BashGrammar.heredoc, """<<EOF
+{
+EOF
+""")
+  println(heredocResult)
+
+
   // use file over reader to provide more informative error messages
   // file must be UTF-8 encoded
-
-  val goodResult: ParseResult[String] = parseAll(BashGrammar.bashBlock, """
+  println("Should succeed:")
+  val goodResult: ParseResult[BashCode] = parseAll(BashGrammar.bashBlock, """
 #!/usr/bin/env bash
 x=$y
-echo 'hi'
+( # stuff in parens launches subprocess
+  x=${y}
+  echo 'hi' # let's confuse the parser: {
+  z='confusing string {'
+)
+y="another confusing string{"
+string_with_var="$doubleQuoted"
 function ohai {
-  $0
+  $a0
 }
+$moses < $in > $out
+pid=$$
+subproc=$(echo $(echo $hi))
+string_len=${#pid}
+string_manip=${$pid:0:5}
+cat <<EOF
+{
+EOF
 """
 )
   println(goodResult)
+  println("Variable list: " + goodResult.get.vars)
 
-  val badResult: ParseResult[String] = parseAll(BashGrammar.bashBlock, """
+
+println("Let's fail:")
+  val badResult: ParseResult[BashCode] = parseAll(BashGrammar.bashBlock, """
 #!/usr/bin/env bash
 x=$y
-echo 'hi'
+echo 'hi' # let's confuse it: {
 function ohai {
-  $0
+  $a0
 """
 )
   println(badResult)
 
+}
+
+// TODO: Pass a StringBuilder down through the AST to make stringification faster
+class BashCode(val code: String, val vars: Set[String] = Set.empty) {
+  override def toString = code
 }
 
 /**
@@ -46,52 +76,121 @@ function ohai {
  */
 object BashGrammar {
 
+  // "... there are dark corners in the Bourne shell, and people use all of them."
+  // --Chet Ramey
+
   import BashParser._
 
   // === WHITE SPACE ===
 
   /** End of line characters, including end of file. */
-  val eol: Parser[String] = literal("\r\n") | literal("\n") | regex("""\z""".r) | literal(CharArrayReader.EofCh.toString) 
+  val eol: Parser[String] = literal("\r\n") | literal("\n") | regex("""\z""".r) | literal(CharArrayReader.EofCh.toString)
+
   /** Non-end of line white space characters */
   val space: Parser[String] = regex("""[ \t]+""".r)
-  
-  /**
-   * End of line, optionally followed by more whitespace, followed by another end of line. 
-   * <p>
-   * This second end of line is recognized, but not consumed.
-   */
-  val emptyLine = (literal("\r\n") | literal("\n")) ~ regex("""[ \t]*""".r) ~ guard(eol)
-  
-  /** Sequence of empty lines. */
-  val emptyLines = emptyLine*
-    
-  /** Non-white space sequence. */
-  val nonSpace: Parser[String] = regex("""[^\r\n \t]+""".r)
-     
-  // === COMMENTS ===
 
-  /** A single line of comment. */
-  val comment: Parser[String] = 
-    """[ \n\r\t]*#[ \t]*""".r ~> commentContent <~ guard(eol) //| failure("Expected a comment line, but didn't find one.")
+  /** A single line of comment. It is paired in that the # and \n bound the comment */
+  val comment: Parser[BashCode] = (regex("""[ \n\r\t]*#[ \t]*""".r) ~ commentContent ~ eol) ^^ {
+    case re ~ content ~ e => new BashCode(re + content + e)
+  }
 
   /** The content portion of a single line of comment. 
    *  Notably, this excludes the syntactic comment marker itself. 
    */
   val commentContent: Parser[String] = """[^\r\n]*""".r
 
-  // === NAMES & VALUES ===
+  def singleQuoteStringLiteral: Parser[BashCode] = (literal("'") ~ regex("""[^']*""".r) ~ literal("'")) ^^ {
+    case open ~ str ~ close => new BashCode(open + str + close)
+  }
 
-  /** Name of a variable, possibly followed by an equals sign.
-   *  <p>
-   *  The name must conform to Bash variable name requirements: 
-   *  "A word consisting solely of letters, numbers, and underscores, and beginning with a letter or underscore."
-   *  We make an exception for parameter names, which may be prefixed with a dot, to indicate that they are
-   *  Resource Parameters, for use with submitters rather than by the task's bash code
-   */
-  def bashBlock: Parser[String] = {
-    regex("""[^{}]*""".r) ~ opt( literal("{") ~ bashBlock ~ literal("}") ~ regex("""[^{}]*""".r)) ^^ {
-      case before ~ None => before
-      case before ~ Some(open ~ b ~ close ~ after) => before + "{" + b + "}" + after
+  def doubleQuoteStringLiteral: Parser[BashCode] = (literal("\"") ~ doubleQuoteContent ~ literal("\"")) ^^ {
+    case open ~ dq ~ close => new BashCode(open + dq + close, dq.vars)
+  }
+
+  def doubleQuoteContent: Parser[BashCode] = regex("[^$\"]*".r) ~ opt(doubleQuoteSpecialContent ~ doubleQuoteContent) ^^ {
+    case before ~ None => new BashCode(before)
+    case before ~ Some(special ~ after) => new BashCode(before + special + after, special.vars ++ after.vars)
+  }
+  
+  def doubleQuoteSpecialContent: Parser[BashCode] = variableLike | stringLiteral
+
+  def stringLiteral: Parser[BashCode] = singleQuoteStringLiteral | doubleQuoteStringLiteral
+
+  // never match any paired elements
+  // and special case << for heredocs
+  def codeBlob: Parser[String] = (regex("[^{}()\"'#$<]*".r) ~ opt(regex("<[^<]".r) ~ codeBlob)) ^^ {
+    case blob ~ None => blob
+    case blob1 ~ Some(re ~ blob2) => blob1 + re + blob2
+  }
+
+  def curlySection: Parser[BashCode] = (literal("{") ~ bashBlock ~ literal("}")) ^^ {
+    case open ~ b ~ close => new BashCode(open + b + close, b.vars)
+  }
+
+  def parenSection: Parser[BashCode] = (literal("(") ~ bashBlock ~ literal(")")) ^^ {
+    case open ~ b ~ close => new BashCode(open + b + close, b.vars)
+  }
+
+  // named variables $hi and ${hi}, builtin variable $$ or $? or $!, command substitution $(echo), etc.
+  // NOTE: String manipulation must be matched *after* variable
+  def variableLike: Parser[BashCode] = internalVariable | commandSub | variable | stringManipulation
+
+  // command substitution: $(echo)
+  def commandSub: Parser[BashCode] = (literal("$(") ~ bashBlock ~ literal(")")) ^^ {
+    case open ~ b ~ close => new BashCode(open + b + close, b.vars)
+  }
+
+  /* see http://tldp.org/LDP/abs/html/internalvariables.html
+   * we intentially don't support script parameters: $@ $* $- $# $0 $1, etc. */
+  def internalVariable: Parser[BashCode] = (literal("$$") | literal("$?") | literal("$!") | literal("$_")) ^^ {
+    case x => new BashCode(x)
+  }
+
+  /* see http://tldp.org/LDP/abs/html/string-manipulation.html
+   * string manipulations can contain arbitrary regular expressions
+   * we don't match any variables inside this mess */
+  def stringManipulation: Parser[BashCode] = (literal("${") ~ regex("[^}]+".r) ~ literal("}")) ^^ {
+    case open ~ content ~ close => new BashCode(open + content + close)
+  }
+
+  def variable: Parser[BashCode] = (literal("$") ~ opt(literal("{")) ~ variableName ~ opt(literal("}"))) ^^ {
+    case dollar ~ None ~ name ~ None => new BashCode(dollar + name, Set(name))
+    case dollar ~ Some(open) ~ name ~ Some(close) => new BashCode(dollar + open + name + close, Set(name))
+  }
+
+  def variableName: Parser[String] = regex("[A-Za-z_][A-Za-z0-9_]*".r)
+
+  def heredoc: Parser[BashCode] = (literal("<<") ~ opt(literal("-")) ~ heredocMarker ~ eol ~ heredocContent ~ literal("EOF") ~ eol) ^^ {
+    case double ~ None ~ marker1 ~ e1 ~ hdoc ~ eof ~ e2  => new BashCode(double + marker1 + e1 + hdoc + eof + e2)
+    case double ~ dash ~ marker1 ~ e1 ~ hdoc ~ eof ~ e2 => new BashCode(double + dash + marker1 + e1 + hdoc + eof + e2)
+  }
+
+  // TODO: Throw an error if user tries to use anything other than EOF
+  def heredocMarker: Parser[String] = literal("EOF")
+
+  def heredocLine: Parser[String] = not(literal("EOF")) ~> regex("""[^\r\n]*\n""".r)
+
+/*
+  def heredocContent: Parser[String] = rep(heredocLine) ^^ {
+    case list => list.mkString("")
+  }
+*/
+  def heredocContent: Parser[String] = literal("{\n")
+
+  def nonBlobElement: Parser[BashCode] = variableLike | parenSection | curlySection | stringLiteral | comment | heredoc
+
+// Done: Strings
+// Done: Comments
+// Done: Variables
+// Done: Heredocs (Restricted to a single non-arbitrary EOF marker)
+// Done: Process substitutions
+// Done: Variable mangling and string operations
+// TODO: Vars in double quote strings
+// Done: Pass up list of variables so that we can see if they're declared or unused (Warn if not)
+  def bashBlock: Parser[BashCode] = {
+     codeBlob ~ opt(nonBlobElement ~ bashBlock) ^^ {
+      case before ~ None => new BashCode(before)
+      case before ~ Some(nonblob ~ after) => new BashCode(before + nonblob + after, nonblob.vars ++ after.vars)
     }
   }
 }
