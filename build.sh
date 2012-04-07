a9c4a8faf69cfb9dadbe4b7e442cf6144e48d5e1
Jonathan Clark <jon.h.clark@gmail.com>
2012-04-07 14:53:07 -0400

    Some library additions

--- build.sh
+++ build.sh
@@ -5,11 +5,19 @@ libDir=$scriptDir/lib
 
 $scriptDir/get_deps.sh
 
+libs=$libDir/scala-optparse-1.1.jar
+libs=$libs:$libDir/scalatest-1.6.1.jar
+libs=$libs:$libDir/sqlitejdbc-v056.jar
+libs=$libs:$libDir/servlet-api-3.0.jar
+libs=$libs:$libDir/jetty-all-8.0.4.v20111024.jar
+#libs=$libs:$libDir/scala-io-core_2.9.1-0.3.0.jar
+#libs=$libs:$libDir/scala-io-file_2.9.1-0.3.0.jar
+
 echo >&2 "Building source..."
 mkdir -p $scriptDir/bin
 find $scriptDir/src/main/scala \
   | egrep '\.scala$' \
-  | xargs fsc -cp $libDir/scalatest-1.6.1.jar:$libDir/sqlitejdbc-v056.jar:$libDir/servlet-api-3.0.jar:$libDir/jetty-all-8.0.4.v20111024.jar  \
+  | xargs fsc -deprecation -cp $libs \
     -d $scriptDir/bin/ \
   | $scriptDir/color_scalac.awk
 
