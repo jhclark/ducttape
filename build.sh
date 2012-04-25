516ae554235d3943944a2e53ddf04aa490509b28
Jonathan Clark <jon.h.clark@gmail.com>
2012-04-25 10:08:28 -0400

    Add scala.timings, which will give per-file timings in 2.10

--- build.sh
+++ build.sh
@@ -16,7 +16,9 @@ echo >&2 "Building source..."
 mkdir -p $scriptDir/bin
 find $scriptDir/src/main/scala \
   | egrep '\.scala$' \
-  | xargs fsc -deprecation -cp $libs  \
+  | xargs scalac \
+    -Dscala.timings=true \
+    -unchecked -deprecation -cp $libs  \
     -d $scriptDir/bin/ \
   | $scriptDir/color_scalac.awk
 
