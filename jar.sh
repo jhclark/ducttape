a1daa283fb1c956c3e3fbb5b4072bbb9724ad57f
Jonathan Clark <jon.h.clark@gmail.com>
2012-04-25 13:07:40 -0400

    Manufacture a JAR file for running regression tests on Travis

--- jar.sh
+++ jar.sh
@@ -3,4 +3,4 @@ set -ueo pipefail
 scriptDir=$(scala -e 'println(new java.io.File("'$(dirname $0)'").getAbsolutePath)')
 
 echo >&2 "Building JAR..."
-(cd $scriptDir/bin; zip -qr $scriptDir/ducttape.jar *)
+(cd $scriptDir/target/scala-2.9.2/classes; zip -qr $scriptDir/ducttape.jar *)
