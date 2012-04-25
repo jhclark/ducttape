bdb7d7a3e4cdbf017b180ff2592943c31f04754d
Jonathan Clark <jon.h.clark@gmail.com>
2012-04-25 13:12:41 -0400

    Use custom run scripts for regression testing when present

--- test-regression.sh
+++ test-regression.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 set -eo pipefail
-scriptDir=$(dirname $0)
+scriptDir=$(readlink -f $(dirname $0))
 
-echo "Running tests from: $dir"
-for dir in dirs; do
-    for tape in syntax/tutorial/*/*.tape; do
-        echo "Running test: $dir"
-        $scriptDir/ducttape $tape -y purge '*' '*'
-        $scriptDir/ducttape $tape -y
-    done
+tutorialDir=$(readlink -f syntax/tutorial)
+for tape in $tutorialDir/*/*.tape; do
+    dir=$(dirname $tape)
+    basefile=$(basename $tape .tape)
+    customSh=$dir/$basefile.sh
+    if [ -e $customSh ]; then
+        # Run via the custom script, which may give additional arguments such as a .conf file
+        CMD=$customSh
+    else
+        # Just directly execute the .tape file with ducttape
+        CMD="$scriptDir/ducttape $tape"
+    fi
+    echo "==================="
+    echo "Running test: $CMD"
+    echo "==================="
+    cd $dir
+    $CMD -y purge '*' '*'
+    $CMD -y
 done
