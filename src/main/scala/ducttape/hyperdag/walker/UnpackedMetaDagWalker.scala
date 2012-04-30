ae19853c6d832203821735f5ef23fb0846b0a27e
Jonathan Clark <jon.h.clark@gmail.com>
2012-04-30 12:46:35 -0400

    Refactored UnpackedMetaDagWalker and added (untested) internal phantom vertices

--- src/main/scala/ducttape/hyperdag/walker/UnpackedMetaDagWalker.scala
+++ src/main/scala/ducttape/hyperdag/walker/UnpackedMetaDagWalker.scala
@@ -33,87 +33,80 @@ class UnpackedMetaDagWalker[V,M,H,E,D,F](
     }
   }
 
-  private val delegate = new UnpackedDagWalker[V,H,E,D,F](
-    dag.delegate, selectionFilter, hedgeFilter, constraintFilter,
-    new DefaultVertexFilter[V,H,E,D], comboTransformer, toD)
+  private val delegate = new UnpackedDagWalker[V,H,E,D,F](dag.delegate, selectionFilter, hedgeFilter, constraintFilter,
+                                                          new DefaultVertexFilter[V,H,E,D], comboTransformer, toD)
 
   // we must be able to recover the epsilon-antecedents of non-epsilon vertices
+  // ...AND phantom-antecedents to support graph-internal phantom vertices
   // so that we can properly populate their state maps
   // unfortunately, this kills space complexity, since we must hold on to these until
   // all members of a packed vertex have been unpacked
   // TODO: Right now, we don't dectect when all members of a packed vertex have
   // been unpacked so that we can reclaim space. Could we refcount them?
-  private val epsilons = new mutable.HashMap[(PackedVertex[V],Seq[D]), UnpackedVertex[V,H,E,D]]
-
+  private val skips = new mutable.HashMap[(PackedVertex[V],Seq[D]), UnpackedVertex[V,H,E,D]]
+  
+  // after getNextMeta skips over any phantoms and epsilons, we must then
+  // re-map the parent realizations of the metavertex to be returned, which involves
+  // tracing backward over any epsilon and phantom vertices
+  private def followPhantomChain(prev: Seq[PackedVertex[V]]): Seq[PackedVertex[V]] = prev match {
+    case Nil => Nil
+    case Seq(single) if (dag.shouldSkip(single)) => followPhantomChain(dag.delegate.parents(single))
+    case multiple => {
+      if (multiple.exists(v => dag.shouldSkip(v))) {
+        throw new RuntimeException("Found multiple parents while re-mapping parents of a meta vertex. " +
+        		"However, one of those parents is an epsilon or phantom vertex.")
+      }
+      multiple // good, we've found our final parents
+    }
+  }
+  
+  private def mapParents(raw: UnpackedVertex[V,H,E,D]): Option[UnpackedMetaVertex[V,H,E,D]] = {  
+    // TODO: CAN WE STOP UNPACKING EARLY?
+    
+    val parents: Seq[PackedVertex[V]] = followPhantomChain(Seq(raw.packed))
+    assert(parents.size == raw.parentRealizations.size,
+           "Parent size %d != parentReal.size %d".format(parents.size, raw.parentRealizations.size))
+           
+    // these lists are both parallel to the number of incoming metaedges
+    val activeEdges = new mutable.ListBuffer[HyperEdge[H,E]]
+    val metaParentReals = new mutable.ListBuffer[Seq[Seq[D]]]
+           
+    // for: parallel to number of incoming meta edges
+    for ( (parentEpsilonV: PackedVertex[_], parentEpsilonReals: Seq[Seq[D]]) <- parents.zip(raw.parentRealizations)) {
+      val parentEpsilonUV: UnpackedVertex[V,H,E,D] = skips( (parentEpsilonV, parentEpsilonReals) )
+      // use this Seq[Seq[D]], which is parallel to the active hyperedge
+      activeEdges += parentEpsilonUV.edge.get
+      metaParentReals += parentEpsilonUV.parentRealizations
+    }
+    Some(new UnpackedMetaVertex[V,H,E,D](raw.packed, activeEdges, raw.realization, metaParentReals, raw))
+  }
+  
+  private def getNextDelegate(): Option[UnpackedVertex[V,H,E,D]] = delegate.take() match {
+    case Some(uv) if (dag.shouldSkip(uv.packed)) => {
+      //println("TAKE SKIPPING: " + result)
+      delegate.complete(uv)
+      if (dag.shouldSkip(uv.packed)) {
+        skips += (uv.packed, uv.realization) -> uv // TODO: We'd really prefer not to store these...
+      }
+      getNextDelegate()
+    }
+    case opt => opt
+  }
+  
+  private def getNextMeta(): Option[UnpackedMetaVertex[V,H,E,D]] = getNextDelegate() match {
+    case None => None
+    case Some(raw: UnpackedVertex[_,_,_,_]) => mapParents(raw)
+  }
+  
   override def complete(item: UnpackedMetaVertex[V,H,E,D], continue: Boolean = true) = {
     delegate.complete(item.dual, continue)
   }
 
-  override def take(): Option[UnpackedMetaVertex[V,H,E,D]] = {
-
-    def getNext(): Option[UnpackedMetaVertex[V,H,E,D]] = {
-      var result: Option[UnpackedVertex[V,H,E,D]] = delegate.take()
-      // never return epsilon vertices nor phantom verties
-      // we're guaranteed to only have one epsilon vertex in between vertices (no chains)
-      // but phantom vertices break this
-      while (!result.isEmpty && dag.shouldSkip(result.get.packed)) {
-        //println("TAKE SKIPPING: " + result)
-        val uv = result.get
-        delegate.complete(uv)
-        if (dag.isEpsilon(uv.packed)) {
-          // TODO: We'd really prefer not to store these...
-          epsilons += (uv.packed, uv.realization) -> uv
-        }
-        result = delegate.take()
-      }
-      //println("TAKING: " + result)
-      
-      return result match {
-        case None => None
-        case Some(raw: UnpackedVertex[_,_,_,_]) => {
-          
-          // TODO: CAN WE STOP UNPACKING EARLY?
-          
-          // these lists are both parallel to the number of incoming metaedges
-          val activeEdges = new mutable.ListBuffer[HyperEdge[H,E]]
-          val metaParentReals = new mutable.ListBuffer[Seq[Seq[D]]]
-          
-          def followPhantomChain(prev: Seq[PackedVertex[V]]): Seq[PackedVertex[V]] = {
-            dag.delegate.parents(v) match {
-                case Nil => None // skip the case of phantom parents if phantom has in-degree zero
-                case Seq(singleParent) if (dag.isPhantom(singleParent)) => { // get *its* parent if it has in-degree == 1
-                  // this parent will be an epsilon -- then get *ITS* parent
-                  followPhantomChain(singleParent)
-                }
-                case _ => {
-                  // phantom vertices with in-degree > 1 are disallowed (to maintain constraint that all metaedges are active)
-                  throw new RuntimeException("Phantom vertices with in-degree > 1 are not allowed")
-                }
-              }
-          }
-          
-          val directParents: Seq[PackedVertex[V]] = dag.delegate.parents(raw.packed)
-          val parents: Seq[PackedVertex[V]] = followPhantomChain(directParents)
-          assert(parents.size == raw.parentRealizations.size,
-                 "Parent size %d != parentReal.size %d".format(parents.size, raw.parentRealizations.size))
-          // for: parallel to number of incoming meta edges
-          for ( (parentEpsilonV: PackedVertex[_], parentEpsilonReals: Seq[Seq[D]]) <- parents.zip(raw.parentRealizations)) {
-            val parentEpsilonUV: UnpackedVertex[V,H,E,D] = epsilons( (parentEpsilonV, parentEpsilonReals) )
-            // use this Seq[Seq[D]], which is parallel to the active hyperedge
-            activeEdges += parentEpsilonUV.edge.get
-            metaParentReals += parentEpsilonUV.parentRealizations
-          }
-          Some(new UnpackedMetaVertex[V,H,E,D](raw.packed, activeEdges, raw.realization, metaParentReals, raw))
-        }
-      }
-    } // getNext
-      
-    var result: Option[UnpackedMetaVertex[V,H,E,D]] = getNext()
-    while (result != None && !vertexFilter(result.get)) {
-      //System.err.println("MEAT Vertex filter does not contain: " + result.get)
-      complete(result.get, continue=false)
-      result = getNext()
+  override def take(): Option[UnpackedMetaVertex[V,H,E,D]] = getNextMeta() match {
+    case Some(candidate) if (!vertexFilter(candidate)) => {
+      complete(candidate, continue=false)
+      take()
     }
-    result
-  } // take
+    case opt => opt 
+  }
 }
