8d689a97b6027395f75a21babd9683eb393b7d0a
Jonathan Clark <jon.h.clark@gmail.com>
2012-05-24 10:46:07 -0400

    Order is important when removing partial output

--- src/main/scala/ducttape/exec/PartialOutputMover.scala
+++ src/main/scala/ducttape/exec/PartialOutputMover.scala
@@ -13,6 +13,8 @@ import grizzled.slf4j.Logging
 
 /**
  * Moves incomplete or invalidated tasks to the attic before re-running them.
+ * The CompletionChecker has already discovered which tasks are broken or partially completed.
+ * 
  * partial is a list of the task/realizations that have partial output that needs to be moved
  *         (and are guaranteed to have version numbers)
  * broken is a list of the task/realizations that are partial but are missing information
@@ -26,20 +28,19 @@ class PartialOutputMover(dirs: DirectoryArchitect,
     
     debug("Considering %s".format(task))
     
-    if (partial( (task.name, task.realization) )) {
-      val taskEnv = new TaskEnvironment(dirs, task)
-      val version = TaskVersion.read(taskEnv.versionFile)
-      
-      System.err.println("Moving %s to the attic".format(task))
-      val origDir = dirs.assignDir(task)
-      val atticDir = dirs.assignAtticDir(task, version)
-      Files.moveDir(origDir, atticDir)
-    }
-    
     if (broken( (task.name, task.realization) )) {
       System.err.println("Removing broken partial output for %s".format(task))
       val origDir = dirs.assignDir(task)
       Files.deleteDir(origDir)
+      
+    } else if (partial( (task.name, task.realization) )) {
+      System.err.println("Moving %s to the attic".format(task))
+      val taskEnv = new TaskEnvironment(dirs, task)
+      val version = TaskVersion.read(taskEnv.versionFile)
+      val origDir = dirs.assignDir(task)
+      val atticDir = dirs.assignAtticDir(task, version)
+      Files.moveDir(origDir, atticDir)
     }
+
   }
 }
