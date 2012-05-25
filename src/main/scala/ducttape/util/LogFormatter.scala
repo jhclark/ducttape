23dba3828b65f1d8aa64d3be0e163c3f2a5841f5
Jonathan Clark <jon.h.clark@gmail.com>
2012-05-25 09:00:01 -0400

    Include proper thread name in logging output

--- src/main/scala/ducttape/util/LogFormatter.scala
+++ src/main/scala/ducttape/util/LogFormatter.scala
@@ -8,35 +8,18 @@ import collection.JavaConversions._
 import java.lang.management.ManagementFactory
 
 class LogFormatter extends Formatter {
-  
-    val threadCache = new mutable.HashMap[Long,Thread]
-    threadCache
     
     val startTime = ManagementFactory.getRuntimeMXBean.getStartTime
     
-    def getThreadName(id: Long): String = threadCache.get(id) match {
-      case Some(t) => t.getName
-      case None => {
-        // expand the cache
-        val threads: Iterable[Thread] = Environment.getAllThreads
-        for (t <- threads) {
-          threadCache += t.getId -> t
-        }
-        threadCache.get(id) match {
-          case Some(t) => t.getName
-          case None => id.toString
-        }
-      }
-    }
-  
     override def format(record: LogRecord): String = formatMessage(record) + "\n"
     
     override def formatMessage(record: LogRecord): String = {
+     // NOTE: Thread.currentThread.getId != record.getThreadID   
       "[%d] %s: %s [%s]: %s".format(
           record.getMillis - startTime,
           record.getLevel,
           record.getLoggerName,
-          getThreadName(record.getThreadID),
+          Thread.currentThread.getName,
           record.getMessage)
     }
 }
