a426c21e326f334d8bd655267a5ee6cc8d2efdc7
Jonathan Clark <jon.h.clark@gmail.com>
2012-05-27 12:29:25 -0400

    Fix major bug that resulted in branches being confused for one another. It turns out that all logical operators aren't equivalent.

--- src/main/scala/ducttape/workflow/Branch.scala
+++ src/main/scala/ducttape/workflow/Branch.scala
@@ -8,7 +8,7 @@ trait Branch {
   override def hashCode() = name.hashCode
   override def equals(obj: Any) = obj match { // TODO: Take advantage of pooling
     case that: Branch => {
-      (this.baseline == that.baseline || this.name == that.name) && this.branchPoint == that.branchPoint
+      (this.baseline && that.baseline || this.name == that.name) && this.branchPoint == that.branchPoint
     }
   }
   override def toString() = "%s:%s".format(branchPoint.name, name)
