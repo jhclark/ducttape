e769fa43b142db2a2a4befa47583486f93f729ab
Lane Schwartz <dowobeha@gmail.com>
2012-05-31 14:17:30 -0400

    Added unit tests for bash code parsing

---
+++ src/test/scala/ducttape/syntax/BashCodeTest.scala
@@ -0,0 +1,36 @@
+package ducttape.syntax
+import ducttape.util.AbstractTest
+import ducttape.syntax.GrammarParser.Parser
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class BashCodeTest extends AbstractTest("bash code",BashGrammar.bashBlock) {
+ 
+  def successCases = Set(
+    "",  
+    " ",
+    """A_variable_Name__="hello"""",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890_=5",
+    "$x",
+    "${x}",
+    "${x:+foo}",
+    "${x:+$x}",
+    """echo "export CLASSPATH=${classpath_dir}/antlr-3.1.1.jar:${CLASSPATH:+:$CLASSPATH}" > ${environment}""",
+    "echo ${variable##*/}",
+    """echo "it's ok"""",
+    """echo 'my favorite is the " mark'"""
+
+  ) 
+  
+  def failureCases = Set(
+    "${x",
+    """echo "hello,
+    world"""    
+  ) 
+  
+  def errorCases = Set(
+
+  )
+  
+}
