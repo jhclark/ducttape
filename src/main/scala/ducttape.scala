jhclark/ducttape

commit c111db3b830ef2d05c05148bf9f624a8918e0044

Make .conf syntax equivalent to .tape syntax (Closes #81)

src/main/scala/ducttape.scala
@@ -68,31 +68,58 @@ object Ducttape extends Logging {
     err.println("%sDuctTape v0.2".format(conf.headerColor))
     err.println("%sBy Jonathan Clark".format(conf.byColor))
     err.println(conf.resetColor)
-
+    
     // make these messages optional with verbosity levels?
     debug("Reading workflow from %s".format(opts.workflowFile.getAbsolutePath))
-    val wd: WorkflowDefinition = ex2err(GrammarParser.readWorkflow(opts.workflowFile))
-    val confSpecs: Seq[ConfigAssignment] = ex2err(opts.config_file.value match {
-      case Some(confFile) => {
-        err.println("Reading workflow configuration: %s".format(confFile))
-        GrammarParser.readConfig(new File(confFile))
-      }
-      case None => opts.config_name.value match {
-        case Some(confName) => {
-          wd.configs.find { case c: ConfigDefinition => c.name == Some(confName) } match {
-            case Some(x) => x.lines
-            case None => throw new DucttapeException("Configuration not found: %s".format(confName))
+    val wd: WorkflowDefinition = {
+      val workflowOnly = ex2err(GrammarParser.readWorkflow(opts.workflowFile))
+      
+      val confStuff: WorkflowDefinition = ex2err(opts.config_file.value match {
+        case Some(confFile) => {
+          // TODO: Make sure workflow doesn't have anonymous conf?
+          workflowOnly.anonymousConfig match {
+            case Some(c) => throw new FileFormatException("Workflow cannot define anonymous config block if config file is used", c)
+            case None => ;
           }
+          
+          err.println("Reading workflow configuration: %s".format(confFile))
+          GrammarParser.readConfig(new File(confFile))
+        }
+        case None => new WorkflowDefinition(Nil)
+      })
+      
+      workflowOnly ++ confStuff
+    }
+    
+    val confNameOpt = opts.config_file.value match {
+      case Some(confFile) => Some(Files.basename(confFile, ".conf"))
+      case None => opts.config_name.value match {
+        case Some(confName) => Some(confName)
+        case None => None
+      }
+    }
+    
+    val confSpecs: Seq[ConfigAssignment] = {
+      opts.config_file.value match {
+        // use anonymous conf from config file, if any
+        case Some(_) => wd.anonymousConfig match {
+          case Some(c) => c.lines
+          case None => Nil
         }
         case None => {
-          // use anonymous config, if provided
-          wd.configs.find { case c: ConfigDefinition => c.name == None } match {
-            case Some(x) => x.lines
+          // otherwise, use the conf the user requested, if any
+          confNameOpt match {
+            case Some(name) => {
+              wd.configs.find { case c: ConfigDefinition => c.name == confNameOpt } match {
+                case Some(x) => x.lines
+                case None => throw new DucttapeException("Configuration not found: %s".format(name))
+              }
+            }
             case None => Nil
           }
         }
       }
-    }) ++ wd.globals
+    } ++ wd.globals
     
     val flat: Boolean = ex2err {
       confSpecs.map(_.spec).find { spec => spec.name == "ducttape_structure" } match {
@@ -129,13 +156,6 @@ object Ducttape extends Logging {
               case None => Environment.PWD
             }
           }
-        }
-      }
-      val confNameOpt = opts.config_file.value match {
-        case Some(confFile) => Some(Files.basename(confFile, ".conf"))
-        case None => opts.config_name.value match {
-          case Some(confName) => Some(confName)
-          case None => None
         }
       }
       new DirectoryArchitect(flat, workflowBaseDir, confNameOpt)

src/main/scala/ducttape/syntax/AbstractSyntaxTree.scala
@@ -332,7 +332,10 @@ object AbstractSyntaxTree {
     lazy val tasks: Seq[TaskDef] = taskDefs.filter { t: TaskDef => t.keyword == "task" }
     lazy val packages: Seq[TaskDef] = taskDefs.filter { t: TaskDef => t.keyword == "package" }
     
+    def anonymousConfig: Option[ConfigDefinition] = configs.find(_.name == None)
+    
     override def toString() = blocks.mkString("\n\n")
-  }  
-  
+    
+    def ++(other: WorkflowDefinition): WorkflowDefinition = new WorkflowDefinition(blocks ++ other.blocks)
+  }
 }

src/main/scala/ducttape/syntax/GrammarParser.scala
@@ -36,20 +36,5 @@ object GrammarParser extends RegexParsers {
     }    
   }
   
-  def readConfig(file: File): Seq[ConfigAssignment] = {
-    val result: ParseResult[Seq[ConfigAssignment]] = parseAll(Grammar.configLines, IO.read(file, "UTF-8"))    
-    val pos = result.next.pos
-    
-    return result match {
-      case Success(asses: Seq[ConfigAssignment], _) => {
-       asses.foreach(addFileInfo(_, file))
-       asses 
-      }
-      case Failure(msg, _) =>
-        throw new FileFormatException("ERROR: line %d column %d: %s".format(pos.line, pos.column, msg), file, pos)
-      case Error(msg, _) =>
-        throw new FileFormatException("HARD ERROR: line %d column %d: %s".format(pos.line, pos.column, msg), file, pos)
-    }    
-  }
-  
+  def readConfig(file: File): WorkflowDefinition = readWorkflow(file)
 }

tutorial/01-08-config-extern.conf
@@ -1,4 +1,8 @@
 # An external configuration block
 # All tasks will be placed under an additional top level directory
 # whose name is derived from this config file's name
-someone=world
+#
+# The syntax of external configuration files is equivalent to .tape files 
+config {
+  someone=world
+}

tutorial/01-08-config-extern.sh
@@ -1,1 +1,2 @@
-ducttape 01-08-config-extern.tape -C 01-08-config-extern.conf "$@"
+dir=$(cd $(dirname $0); pwd)
+ducttape $dir/01-08-config-extern.tape -C $dir/01-08-config-extern.conf "$@"

commit 160a346a7b5ef86476f7ae4f9f1fc54bab78599a

Fix bug in which anonymous config blocks failed. Bug was introduced in c111db3

src/main/scala/ducttape.scala
@@ -115,7 +115,10 @@ object Ducttape extends Logging {
                 case None => throw new DucttapeException("Configuration not found: %s".format(name))
               }
             }
-            case None => Nil
+            case None => wd.anonymousConfig match {
+              case Some(c) => c.lines
+              case None => Nil
+            }
           }
         }
       }