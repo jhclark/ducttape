cdaa7d146bed986eb6fee0cc1ea41958315a1b1e
Jonathan Clark <jon.h.clark@gmail.com>
2012-06-23 21:22:07 -0400

    Fix empty.reduceLeft: UnsupportedOperationException by using mkString()

--- src/main/scala/ducttape/exec/TaskEnvironment.scala
+++ src/main/scala/ducttape/exec/TaskEnvironment.scala
@@ -82,16 +82,5 @@ class FullTaskEnvironment(dirs: DirectoryArchitect,
   }
 
   lazy val env = inputs ++ outputs ++ params ++ packageEnvs
-  
-  lazy val taskVariables = {
-    env.map { tuple =>
-    
-      tuple._1 + "=" + tuple._2
-    
-    }.reduceLeft { (acc,line) =>
-      
-      acc + "\n" + line
-      
-    }
-  }
+  lazy val taskVariables = env.map { case (key, value) => "%s=%s".format(key,value) }.mkString("\n")
 }
