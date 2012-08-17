1933d23733bf1699f5e277483a61dda932030035
Lane Schwartz <dowobeha@gmail.com>
2012-08-17 15:22:59 -0400

    Removed stale commented out code

--- src/main/scala/ducttape/syntax/Grammar.scala
+++ src/main/scala/ducttape/syntax/Grammar.scala
@@ -322,20 +322,16 @@ object Grammar {
       howToFailAtStart: (String)=>Parser[Nothing],
       howToFailAtEnd: (String)=>Parser[Nothing]): Parser[String] = {
     ( // If the name starts with an illegal character, bail out and don't backtrack
-      //regex("""[^,"')(\]\[\*\$:@=\s]""".r)<~howToFailAtStart("Illegal character at start of " + title + " name")
       regex("""[^A-Za-z0-9_\-.]""".r)<~howToFailAtStart("Illegal character at start of " + title + " name")
 
       // Else if the name contains only legal characters and the input ends, then parse it
-      //| regex("""[^,"')(\]\[\*\$:@=\s]*$""".r)
       | regex("""[A-Za-z0-9_\-.]*$""".r)
       
       // Else if the name itself is OK, but it is followed by something that can't legally follow the name, bail out and don't backtrack
-      //| regex("""[^,"')(\]\[\*\$:@=\s]*""".r)<~guard(not(regex(whatCanComeNext)))~howToFailAtEnd("Illegal character in " + title + " name. Adding a space after the variable name may fix this error.")
       | regex("""[A-Za-z0-9_\-.]*""".r)<~guard(not(regex(whatCanComeNext)))~howToFailAtEnd("Illegal character in " + title + " name. Adding a space after the variable name may fix this error.")
 
       // Finally, if the name contains only legal characters, 
       //          and is followed by something that's allowed to follow it, then parse it!
-      //| regex("""[^,"')(\]\[\*\$:@=\s]*""".r)
       | regex("""[A-Za-z0-9_\-.]*""".r)
     )
   }
