1c1ce4d51bd352b32f33ba10acc01a2f72070b1a
Jonathan Clark <jon.h.clark@gmail.com>
2012-08-22 13:42:51 -0400

    Be BSD friendly

--- build-support/jar.sh
+++ build-support/jar.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -ueo pipefail
-#scriptDir=$(scala -e 'println(new java.io.File("'$(dirname $0)'").getAbsolutePath)')
-scriptDir=$(readlink -f $(dirname $0))
+scriptDir=$(cd $(dirname $0); pwd)
 rootDir=$scriptDir/..
 
 echo >&2 "Building JAR..."
