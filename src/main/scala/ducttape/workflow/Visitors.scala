f8ab13229b4792d9075b792e22d8f734374c2929
Jonathan Clark <jon.h.clark@gmail.com>
2013-01-16 07:26:00 -0500

    I <3 string interpolation

--- src/main/scala/ducttape/workflow/Visitors.scala
+++ src/main/scala/ducttape/workflow/Visitors.scala
@@ -17,11 +17,11 @@ object Visitors extends Logging {
       numCores: Int = 1,
       traversal: Traversal = Arbitrary): A = {
     
-    debug("Visiting workflow in a(n) " + traversal + " traversal")
-    workflow.unpackedWalker(planPolicy,traversal=traversal).foreach(numCores, { v: UnpackedWorkVert =>
+    debug(s"Visiting workflow using traversal: ${traversal}")
+    workflow.unpackedWalker(planPolicy, traversal=traversal).foreach(numCores, { v: UnpackedWorkVert =>
       val taskT: TaskTemplate = v.packed.value.get
       val task: VersionedTask = taskT.toRealTask(v).toVersionedTask(workflowVersion)
-      debug(s"Visiting $task")
+      debug(s"Visiting ${task}")
       visitor.visit(task)
     })
     visitor
