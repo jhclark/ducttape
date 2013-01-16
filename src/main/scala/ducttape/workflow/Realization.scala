008f14a2e089e61f896fce5a387d17bf86927b75
Jonathan Clark <jon.h.clark@gmail.com>
2013-01-16 07:25:33 -0500

    Use more string interpolation

--- src/main/scala/ducttape/workflow/Realization.scala
+++ src/main/scala/ducttape/workflow/Realization.scala
@@ -11,11 +11,11 @@ import grizzled.slf4j.Logging
 class Realization(val branches: Seq[Branch]) extends Logging {
  // TODO: Keep string branch point names?
 
-  // sort by branch *point* names to keep ordering consistent, then join branch names using dashes
-  //   and don't include our default branch "baseline"
-  // by default we "shorten" realization names by removing branch points using baseline branches
-  //   such that new branch points can easily be added since the entire directory structure doesn't
-  //   suddenly change
+  /** sort by branch *point* names to keep ordering consistent, then join branch names using dashes
+   *   and don't include our default branch "baseline"
+   * by default we "shorten" realization names by removing branch points using baseline branches
+   *   such that new branch points can easily be added since the entire directory structure doesn't
+   *   suddenly change */
   private def realizationName(): String = {
     
     // sort by branch point name and remove references to the baseline branch
@@ -30,7 +30,7 @@ class Realization(val branches: Seq[Branch]) extends Logging {
     // while maintaining semantics
     val names = filteredBranches.map { branch =>
       val displayName = if (branch.baseline) "baseline" else branch.name
-      "%s.%s".format(branch.branchPoint.name, displayName)
+      s"${branch.branchPoint.name}.${displayName}"
     }
     val result = names.mkString(Realization.delimiter)
     if (result.length > 255) {
@@ -42,10 +42,10 @@ class Realization(val branches: Seq[Branch]) extends Logging {
   private def fullRealizationName(hashLongNames: Boolean = true): String = {
     // TODO: Prohibit the branch point name "Baseline"?
     val filteredBranches: Seq[Branch] = branches.filter { _.branchPoint != Task.NO_BRANCH_POINT }
-    val names = filteredBranches.map { branch => "%s.%s".format(branch.branchPoint.name, branch.name) }
+    val names = filteredBranches.map { branch => s"${branch.branchPoint.name}.${branch.name}" }
     val result = names.mkString(Realization.delimiter)
     if (result.length > 255 && hashLongNames) {
-      warn("Very long filename is being hashed: " + result)
+      warn(s"Very long filename is being hashed: ${result}")
       HashUtils.md5(result)
       //throw new RuntimeException("Got realization name longer than 255 characters. This might cause issues on disk: %s".format(result))
     } else {
