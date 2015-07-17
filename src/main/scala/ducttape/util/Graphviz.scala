d36e4c561f8ac2cce63acc1fa39cb6722b393ff0
Lane Schwartz <dowobeha@gmail.com>
2015-07-17 09:32:51 -0500

    Working on new Graphviz/Tikz visualizer.
    Now outputting within-task arrows

--- src/main/scala/ducttape/util/Graphviz.scala
+++ src/main/scala/ducttape/util/Graphviz.scala
@@ -10,131 +10,157 @@ import ducttape.syntax.AbstractSyntaxTree.TaskPackageNames
 import ducttape.syntax.AbstractSyntaxTree.TaskParams
 import ducttape.syntax.AbstractSyntaxTree.WorkflowDefinition
 
-private class IdentifierTracker(private val name:String) {
-  
-  var counter = 0
 
-  def getNextID(item:Object) : String = {
-    counter += 1
-    val id = s"${name}${counter}"
-    
-    if (IdentifierTracker.map.containsKey(item)) {
-      throw new RuntimeException(s"""Cannot assign Item ${item.toString} the ID ${id} because it already has been assigned ID ${IdentifierTracker.map.get(item)}""")
-    } else {
-      IdentifierTracker.map.put(item,id)
-    }
-    
-    return id
-  }
-  
-}
-
-private object IdentifierTracker {
-  
-  val map = new java.util.IdentityHashMap[Object,String]
-  
-  def retrieveID(item:Object) : String = map.get(item)
-  
-}
 
 
 class Graphviz(wd: WorkflowDefinition) {
 
-  private val s = new StringBuilder()
-  
-  
-  private var taskIDs = new IdentifierTracker("task")
-  private var branchIDs = new IdentifierTracker("branch")
-  private var branchPointIDs = new IdentifierTracker("branchPoint")
-    /*
-  private def constructNumberedName(name:String, number:Int) : String = {
-    return s"${name}${number}"
-  }
-  */
-  //private val taskIDmap : Map[TaskDef,String] = {
-    
-  //  val map = new scala.collection.mutable.HashMap[TaskDef,String]()
-    /*
-    wd.blocks.foreach( (block:Block) => {
-    
-      block match { 
-    
-        case task:TaskDef => { taskIDs.getNextID(task) } //constructNumberedName("task", map.size) }
-      
-        case _ => {}
-      
-      }
-    
-    })
-    */
-    //map.toMap
- // }
-  
-  
-  
-  
-  
-  
-  s.append("digraph G {\n\n") // Start directed graph G
-  
-  s.append("\tgraph [nodesep=\"1\", ranksep=\"1\"];\n\n")
+	private class IdentifierTracker(private val name:String, private val map:java.util.IdentityHashMap[Object,String]) {
 
-  /*
-  taskToNumberedName.foreach( (mapEntry:(TaskDef,String)) => {
-    val task = mapEntry._1
-    val string = mapEntry._2
-    s.append('\t')
-    s.append(string)
-    s.append('\n')
-  })
-  */
-  
-  // Iterate over defined blocks
-  wd.blocks.foreach( (block:Block) => {
+		var counter = 0
 
-     block match { 
+		def getID(item:Object) : String = {
+
+		  if (map.containsKey(item)) {
+
+			  return map.get(item)
+
+		  } else {
+
+			  counter += 1
+
+				val id = s"${name}${counter}"
+
+				map.put(item,id)
+
+				return id
+
+			}
+
+		}
+
+	}
+
+  private val map = new java.util.IdentityHashMap[Object,String]
+  
+  
+	
+
+	private var taskIDs = new IdentifierTracker("task", map)
+	private var branchIDs = new IdentifierTracker("branch", map)
+	private var branchPointIDs = new IdentifierTracker("branchPoint", map)
+
+
+	private def drawTaskSubgraph(task:TaskDef, s:StringBuilder) : Unit = {
+
+    val taskID = taskIDs.getID(task)
+
+		s.append(s"""\tsubgraph cluster_${taskID} {\n""")
+		s.append(s"""\t\t${taskID} [style="task" label="${task.name}"]\n""") 
+
+		var outputIDs = new IdentifierTracker(s"${taskID}_out", map)
+		var inputIDs = new IdentifierTracker(s"${taskID}_in", map)
+		var paramIDs = new IdentifierTracker(s"${taskID}_param", map)
+
+		def process(specs:Specs, counter:IdentifierTracker, specType:String) {
+			specs.specs.foreach( (spec:Spec) => {
+				s.append(s"""\t\t${counter.getID(spec)} [style="${specType}" label="${spec.name}"];\n""")
+			})            
+		}
+
+		task.header.children.foreach( (specs:Specs) => {
+			specs match {          
+			case inputs:      TaskInputs       => process(inputs,  inputIDs,  "input")              
+			case outputs:     TaskOutputs      => process(outputs, outputIDs, "output")             
+			case params:      TaskParams       => process(params,  paramIDs,  "param")            
+			case packageNames:TaskPackageNames => {}          
+			}
+		})
+
+		s.append("\t}\n\n") 
     
-        case task:TaskDef => { 
-          
-          val taskID = taskIDs.getNextID(task) //IdentifierTracker.retrieveID(task) //taskIDmap(task)
-          
-          // Output task name
-          s.append(s"""\tsubgraph cluster_${taskID} {\n""")
-          s.append(s"""\t\t${taskID} [style="task" label="${task.name}"]\n""") 
-          
-          var outputIDs = new IdentifierTracker(s"${taskID}_out")
-          var inputIDs = new IdentifierTracker(s"${taskID}_in")
-          var paramIDs = new IdentifierTracker(s"${taskID}_param")
-          
-          def process(specs:Specs, counter:IdentifierTracker, specType:String) {
-            specs.specs.foreach( (spec:Spec) => {
-              s.append(s"""\t\t${counter.getNextID(spec)} [style="${specType}" label="${spec.name}"]\n""")
-            })            
-          }
-          
-          task.header.children.foreach( (specs:Specs) => {
-            specs match {
-              
-              case inputs:TaskInputs   => process(inputs,  inputIDs,  "input")
-              
-              case outputs:TaskOutputs => process(outputs, outputIDs, "output")
-              
-              case params:TaskParams   => process(params,  paramIDs,  "param") 
-              
-              case packageNames:TaskPackageNames => {}
-              
-            }
-          })
-           
-          s.append("\t}\n\n")
+	}
+
+	private def drawTaskArrows(task:TaskDef, s:StringBuilder) : Unit = {
+
+    val taskID = taskIDs.getID(task)
+
+    var outputIDs = new IdentifierTracker(s"${taskID}_out", map)
+    var inputIDs = new IdentifierTracker(s"${taskID}_in", map)
+    var paramIDs = new IdentifierTracker(s"${taskID}_param", map)
+
+    def process(specs:Specs, counter:IdentifierTracker, inputOrParam:Boolean) {
+      specs.specs.foreach( (spec:Spec) => {
+        if (inputOrParam) {
+        	s.append(s"""\t${counter.getID(spec)} -> ${taskID};\n""")
+        } else {
+          s.append(s"""\t${taskID} -> ${counter.getID(spec)};\n""")
         }
-      
-        case _ => {}
-      
+      })            
+    }
+
+    task.header.children.foreach( (specs:Specs) => {
+      specs match {          
+      case inputs:      TaskInputs       => process(inputs,  inputIDs,  true)              
+      case outputs:     TaskOutputs      => process(outputs, outputIDs, false)             
+      case params:      TaskParams       => process(params,  paramIDs,  true)            
+      case packageNames:TaskPackageNames => {}          
+      }
+    })
+    
+    s.append("\n")
+  }
+
+  
+  def graph() : String = {
+    
+    val s = new StringBuilder()
+    
+    s.append("digraph G {\n\n") // Start directed graph G
+
+    s.append("\tgraph [nodesep=\"1\", ranksep=\"1\"];\n\n")
+
+
+    // Iterate over defined blocks
+    wd.blocks.foreach( (block:Block) => {
+
+      block match { 
+
+      case task:TaskDef => { 
+
+        drawTaskSubgraph(task, s)
+        drawTaskArrows(task, s)
+
+      }
+
+      case _ => {}
+
       }    
-  })
+    })
+
+    s.append("}\n\n") // End directed graph G    
+    
+    return s.toString()
+    
+  }
   
-  s.append("}\n") // End directed graph G
   
-  System.out.println(s.toString)
+  def dot2tex() : String = {
+    
+    val s = new StringBuilder()
+    
+    s.append("""\begin{center}""").append('\n')
+    s.append("""\begin{tikzpicture}[>=latex, scale=0.85, transform shape]""").append('\n')
+    s.append("""\begin{dot2tex}[dot,scale=0.85,tikzedgelabels,codeonly]""").append("\n\n")    
+    
+    s.append(graph())
+    
+    s.append("""\end{dot2tex}""").append('\n')
+    s.append("""\end{tikzpicture}""").append('\n')
+    s.append("""\end{center}""").append('\n')    
+    
+    return s.toString()
+    
+  }
+
 }
