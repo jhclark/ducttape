6add99bb4c08db3659464da3008fbb8064f02a7d
Lane Schwartz <dowobeha@gmail.com>
2015-09-23 10:54:44 -0500

    Added license notice to scala files.

--- src/test/scala/SampleTest.scala
+++ src/test/scala/SampleTest.scala
@@ -1,3 +1,5 @@
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import org.scalatest.FlatSpec
 import org.junit.runner.RunWith
 import scala.collection.mutable.Stack
